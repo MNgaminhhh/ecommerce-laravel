0bbef79dc7919fba991ef865d0c11fadd365aa82
MN <57828955+MNgaminhhh@users.noreply.github.com>
2024-06-24 16:48:29 +0700

    first commit

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
+
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+Route::get('profile',[ProfileController::class,'index'])->name('profile');
+Route::post('profile/update',[ProfileController::class,'updateProfile'])->name('profile.update');
